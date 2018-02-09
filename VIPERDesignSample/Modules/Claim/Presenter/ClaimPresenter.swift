78b6864b677c8b47ae5cf4c3f2abd74e523e2226
Nirupama Abraham <niabraha@in.ibm.com>
2018-02-09 15:48:56 +0530

    Added the different VIPER modules.

---
+++ VIPERDesignSample/Modules/Claim/Presenter/ClaimPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  ClaimPresenter.swift
+//  VIPERDesignSample
+//
+//  Created by Nirupama M Abraham on 09/02/18.
+//  Copyright © 2018 Nirupama M Abraham. All rights reserved.
+//
+
+import Foundation
