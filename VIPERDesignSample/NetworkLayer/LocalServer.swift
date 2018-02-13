0bb8e56778e0237e594ed62ea49004fdf8e06b5e
Nirupama Abraham <niabraha@in.ibm.com>
2018-02-13 18:41:36 +0530

    Resolving conflicts from previous merge.

--- VIPERDesignSample/NetworkLayer/LocalServer.swift
+++ VIPERDesignSample/NetworkLayer/LocalServer.swift
@@ -36,17 +36,17 @@ func configureLocalServer() {
     
     // Claim Files
     server.get("/claimsadjust_claimFile/claim_files?adjusterId={adjuster_id}", handler: { (request, parameters) -> Response in
-        if let adjusterId = parameters["adjuster_id"] {
+       // if let adjusterId = parameters["adjuster_id"] {
             return Response(filename: "Claim", ofType: "json")
-        }
+       // }
         return Response()
     })
 
     //Appointments
     server.get("/claimsadjust_appointment/appointments?adjusterId={adjuster_id}", handler: { (request, parameters) -> Response in
-        if let adjusterId = parameters["adjuster_id"] {
+       // if let adjusterId = parameters["adjuster_id"] {
             return Response(filename: "Appointment", ofType: "json")
-        }
+       // }
         return Response()
     })
     
