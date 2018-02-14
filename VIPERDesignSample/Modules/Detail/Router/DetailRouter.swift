4c37f790a9ca06c103124ce6611e01afbbaeadc9
Nirupama Abraham <niabraha@in.ibm.com>
2018-02-14 20:37:56 +0530

    Fixed issue on loading detail view.

--- VIPERDesignSample/Modules/Detail/Router/DetailRouter.swift
+++ VIPERDesignSample/Modules/Detail/Router/DetailRouter.swift
@@ -11,17 +11,15 @@ import UIKit
 
 class DetailRouter: DetailPresenterToRouterProtocol {
     class func createDetailModule(forActivity activity: Activities) -> UIViewController? {
-        if let view = UIViewController.init(nibName: "DetailViewController", bundle: nil) as? DetailViewController {
-            let presenter: DetailViewToPresenterProtocol = DetailPresenter()
-            let router: DetailPresenterToRouterProtocol = DetailRouter()
-            
-            view.presenter = presenter
-            presenter.view = view
-            presenter.router = router
-            presenter.activity = activity
-            
-            return view
-        }
-        return nil
+        let view = DetailViewController(nibName: "DetailViewController", bundle: nil)
+        let presenter: DetailViewToPresenterProtocol = DetailPresenter()
+        let router: DetailPresenterToRouterProtocol = DetailRouter()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.activity = activity
+        
+        return view
     }
 }
